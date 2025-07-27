6fa774d75915207faab399f3aed46207b5f75e54
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-27 02:43:10 -0500

    (setup & creation):  I created and Set up the VehicleSystem, creates engines, wheels, and vehicles.

--- main.cpp
+++ main.cpp
@@ -26,27 +26,4 @@ int main() {
     system.addWheel(new Wheel(19, "sport"));
   }
 
-  // Create vehicles
-  system.addVehicle(new Car("Toyota", "Camry", 2022, "sedan"));
-  system.addVehicle(new Truck("Ford", "F-150", 2020, 12000));
-  system.addVehicle(new Motorcycle("Harley-Davidson", "Street 750", 2021, "cruiser"));
-
-  // Install engines
-  system.installEngine(0, 1);  // Sedan gets I4
-  system.installEngine(1, 0);  // Truck gets V8
-  system.installEngine(2, 2);  // Bike gets V-Twin
-
-  // Install wheels
-  for (int i = 0; i < 4; i++) system.installWheel(0, i);  // Sedan wheels
-  for (int i = 0; i < 6; i++) system.installWheel(1, 10 + i); // Truck wheels
-  for (int i = 0; i < 2; i++) system.installWheel(2, 16 + i); // Bike wheels
-
-  // Demonstrate system
-  std::cout << "\nAll Vehicles in System:\n";
-  system.displayAllVehicles();
-
-  std::cout << "\nStarting All Vehicles:\n";
-  system.startAllVehicles();
-
-  return 0;
-}
+  
